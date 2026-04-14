b80c4dc11cf682446342c53d6da4574b0d093ee7
Chandan <cgarg2461@gmail.com>
2026-04-14 13:31:07 +0530

    Secure our app with arcjet

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
+import securityMiddleware from './middleware/security.middleware.js';
 
 const app = express();
 
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
+
+app.use(securityMiddleware);
 
 // morgan will basically log all the HTTP requests and this below line will log all the messages to the logger file
 app.use(
---
+++ src/config/arcjet.js
@@ -0,0 +1,19 @@
+import { openai } from '@ai-sdk/openai';
+import arcjet, { detectBot, slidingWindow, shield } from '@arcjet/next';
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({ mode: 'LIVE' }),
+    detectBot({
+      mode: 'LIVE',
+      allow: ['CATEGORY:SEARCH_ENGINE', 'CATEGORY:PREVIEW'],
+    }),
+    slidingWindow({
+      mode: 'LIVE',
+      interval: '2s',
+      max: 5,
+    }),
+  ],
+});
+export default aj;
